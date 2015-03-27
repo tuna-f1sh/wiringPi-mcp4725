fbb92ce64ad85d34513e4eb6dc2c9f7681035aa3
John Whittington <git@jbrengineering.co.uk>
2015-03-27 12:12:49 +0000

    Added driver for ADS1115 16bit ADC

---
+++ examples/ads1115_read.c
@@ -0,0 +1,29 @@
+/*
+ * ADS1115 driver example
+ *
+ * March 2015 John Whittington http://www.jbrengineering.co.uk @j_whittington
+ *
+*==============================================================*/
+
+#include <wiringPi.h>
+#include <ads1115.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[]) {
+  int16_t value;
+  double voltage;
+
+  ads1115Setup(100,0x48);
+
+  for (;;) {
+    value = (int16_t) analogRead(100);
+    voltage = value * (4.096 / 32768);
+
+    printf("ADS1115 Reading: %d\n\r",value);
+    printf("ADS1115 Voltage: %g\n\r",voltage);
+    delay(1000);
+  }
+
+  return 0;
+}
